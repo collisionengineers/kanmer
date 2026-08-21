050877d1328b5a9cc0209edbfb37851bf66ccf70
collisionengineers <digital@collisionengineers.co.uk>
2026-08-21 01:59:55 +0100

    feat(core): add browser-safe export

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/browser.ts"],
+  splitting: false,
   format: ["esm"],
   dts: true,
   clean: true,
